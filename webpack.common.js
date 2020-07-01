3e858aa1ec32e3e91f2e340b12db6bfa0bff421d
abdhilabs <rizaabdhi@gmail.com>
2020-07-01 11:37:11 +0700

    rebuild

--- webpack.common.js
+++ webpack.common.js
@@ -3,7 +3,7 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-    entry: "./src/app.js",
+    entry: "/src/app.js",
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js"
